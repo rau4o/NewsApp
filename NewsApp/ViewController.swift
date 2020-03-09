f2ffadd13e3c06ad1bd64df093ab2c06a5e76492
Solly <sollydevapp@gmail.com>
2020-03-09 15:03:03 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by rau4o on 3/9/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
